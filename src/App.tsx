5ec0026e761b44e351595593d2eae14aa58fdaa0
HansaSandamal <hansasandamal@gmail.com>
2021-05-23 20:53:06 +0530

    create gallery page

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import Header from "./layout/Header";
 import './assets/style_sheets/Main.scss';
 import Home from "./view/Home";
 import Footer from "./layout/Footer";
+import Gallery from "./view/Gallery";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Home/>
+            <Gallery/>
             <Footer/>
         </div>
     );
