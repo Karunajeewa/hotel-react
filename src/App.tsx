fecc81cf840f444d251ddf0c866b641462404781
Chamodi Jayodya <jayodyachamodi@gmail.com>
2021-05-23 16:04:10 +0530

    Create footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from "./layout/Header";
 import './style_sheets/Main.scss';
+import Footer from "./layout/Footer";
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <p>Body of the pages</p>
+            <Footer/>
         </div>
     );
 }
